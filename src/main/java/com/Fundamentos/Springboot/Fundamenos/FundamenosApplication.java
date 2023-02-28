20db7412f2e35096912bc25a93857ec3aabaa300
Thelele99 <esneidergil99@gmail.com>
2023-02-28 09:59:36 -0500

    Mi primer proyecto

---
+++ src/main/java/com/Fundamentos/Springboot/Fundamenos/FundamenosApplication.java
@@ -0,0 +1,13 @@
+package com.Fundamentos.Springboot.Fundamenos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamenosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamenosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Fundamentos/Springboot/Fundamenos/FundamenosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Fundamentos.Springboot.Fundamenos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamenosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
